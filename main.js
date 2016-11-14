a573a132b01286e98739d4efea227cbafb6c68c5
AndyRightNow <naruto950726@hotmail.com>
2016-11-14 20:56:40 +0800

    Reorganize files

--- main.js
+++ main.js
@@ -4,27 +4,17 @@
 const fs = require("fs");
 const log = console.log;
 const walk = require("./walk");
-const distHTML = require("./distHTML");
 const path = require("path");
-const CommandsRunner = require("./commands-runner");
-const benchmark = require("./benchmark");
-const RequestPromise = require("./request-promise");
-const packer = require("./packer/packer");
-const TableTreeNode = require("./deps/table-tree");
-const inlineImports = require("./utils/inliner").inlineImports;
-const extract = require("./utils/extract");
-const TaskRunner = require("./task-runner/task-runner");
-const stripComments = require("./utils/strip-comments");
+// const distHTML = require("./distHTML");
+// const CommandsRunner = require("./commands-runner");
+// const benchmark = require("./benchmark");
+// const RequestPromise = require("./request-promise");
+// const packer = require("./packer/packer");
+// const TableTreeNode = require("./deps/table-tree");
+// const inlineImports = require("./utils/inliner").inlineImports;
+// const extract = require("./utils/extract");
+// const TaskRunner = require("./task-runner/task-runner");
+// const stripComments = require("./utils/strip-comments");
 
 // Constants
 const TEST_ROOT_DIR = path.resolve(__dirname, "./test");
-
-var p = path.resolve(TEST_ROOT_DIR, "stripComments-test", "test.js");
-
-TaskRunner
-  .text(p)
-  .task(stripComments)
-  .output({
-    dir: "./dist",
-    name: "test.dist.js"
-  });
--- replace/index.js
+++ replace/index.js
@@ -9,8 +9,9 @@ const fs = require("fs");
  *
  * @param {string} options.path Path name of the HTML file name to process
  * @param {string} options.content Content of the HTML file name to process
- * @param {string} options.match A substr or regexp to match
- * @param {RegExp} options.newSubstr A new substr to replace the matched old one
+ * @param {string/RegExp} options.match A substr or regexp to match
+ * @param {string} options.newSubstr A new substr to replace the matched old one
+ * @param {function} options.newSubstrFunc A new substr function to produce the string to replace the matched old one
  */
 function replace(options) {
   if (typeof options === "undefined" ||
@@ -18,7 +19,7 @@ function replace(options) {
     throw new Error("Invalid arguments!");
   }
 
-  options.content = options.content.replace(options.match, options.newSubstr);
+  options.content = options.content.replace(options.match, options.newSubstr || options.newSubstrFunc);
 }
 
 module.exports = replace;
